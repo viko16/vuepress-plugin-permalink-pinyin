6f91fa5ced3fa217718f6604dfe6636803f02172
viko16 <16viko@gmail.com>
2019-02-03 12:13:24 +0800

    feat: convert permalink to pinyin

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = {
+const slugify = require('transliteration').slugify
 
+module.exports = pluginConfig => {
+  return {
+    // https://vuepress.vuejs.org/plugin/option-api.html#extendpagedata
+    extendPageData ($page) {
+      // $page.path was encoded by VuePress already.
+      // Make sure original so I decode it once.
+      const originPagePath = decodeURIComponent($page.path)
+
+      // https://github.com/andyhu/transliteration#slugifystr-options
+      const options = Object.assign({}, pluginConfig, { ignore: ['/', '.'] })
+
+      $page.path = slugify(originPagePath, options)
+    }
+  }
 }
